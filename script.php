967ad417d0a1094ad364a720e094e911037a1ded
Emerson1796 <58262627+Emerson1796@users.noreply.github.com>
2020-02-12 00:32:42 -0300

    first commit

---
+++ script.php
@@ -0,0 +1,15 @@
+<?php
+
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoValidacao.php";
+include "servicos/servicoCategoriaCompetidor.php";
+
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+defineCategoriaCompetidor($nome, $idade);
+
+header('location: index.php');
+
+?>
